2ab180747e8442bd19f2a2b18972bb69d647e8a0
GauthamPrabhuM <gauthamprabhu9@gmail.com>
2023-12-25 10:07:18 +0530

    Modified

--- src/Components/About/About.jsx
+++ src/Components/About/About.jsx
@@ -4,29 +4,29 @@ import me from "../../Assets/me.png";
 import me2 from "../../Assets/me2.png";
 import { MdSimCardDownload } from "react-icons/md";
 import resume from "../../Assets/GauthamPrabhuMResume.pdf";
+
 export default function About(props) {
   return (
-    <>
-      <div className="about_main" id={props.light ? "lightid" : null}>
-        <div className="about_header" id="about">
-          <span className="letter">W</span>
-          <span className="letter">h</span>
-          <span className="letter">o</span>
-          <span className="letter"> </span>
-          <span className="letter">a</span>
-          <span className="letter">m </span>
-          <span className="letter"> </span>
-          <span className="letter">I </span>
-          <span className="letter">? </span>
-        </div>
+    <div className="about_main" id={props.light ? "lightid" : null}>
+      <div className="about_header" id="about">
+        <span className="letter">W</span>
+        <span className="letter">h</span>
+        <span className="letter">o</span>
+        <span className="letter"> </span>
+        <span className="letter">a</span>
+        <span className="letter">m </span>
+        <span className="letter"> </span>
+        <span className="letter">I </span>
+        <span className="letter">? </span>
+      </div>
+      <div className="about_content">
         <div className="left">
-          <img src={props.light ? me2 : me} className="me" id={props.light?'light_me':null} />
-          <div className="leftBg" id={props.light ? 'light_imgBg':null}></div>
+          <img src={props.light ? me2 : me} className="me" id={props.light ? 'light_me' : null} alt="Gautham" />
         </div>
         <div className="about_txt">
           Hi! I am Gautham Prabhu M, a Computer Science Undergrad at Manipal
-          Institue of Technology.I am passionate about my work a deep fascination for the ever-expanding field of Computer Science. 
-          I love collaborating with others, so feel free to reach out
+          Institute of Technology. I am passionate about my work with a deep fascination for the ever-expanding field of Computer Science. 
+          I love collaborating with others, so feel free to reach out.
           <br />
           <a href={resume} download>
             <button
@@ -42,6 +42,6 @@ export default function About(props) {
           </a>
         </div>
       </div>
-    </>
+    </div>
   );
 }
